0ea455640b5982a282572c5dd565c5c480a736ea
choo00412 <118342503+choo00412@users.noreply.github.com>
2026-05-10 20:28:25 +0900

    다운로드 문제 해결

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
+const CACHE_NAME = 'tier-maker-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
+// 앱 설치 시 파일들 캐싱(저장)
 self.addEventListener('install', (e) => {
-  console.log('서비스 워커 설치 완료');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
+// 오프라인일 때 저장된 파일 불러오기
 self.addEventListener('fetch', (e) => {
-  // 오프라인 캐싱 로직이 들어가는 곳
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
